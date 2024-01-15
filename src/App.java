8096ccb3ac20d835d65c08673f0d2658eb11eb3a
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-01-15 12:08:58 +0200

    test commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int x = 1;
+
+        System.out.println(x);
     }
 }
